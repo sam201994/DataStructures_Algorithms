c708f9a579067a039836097efa0605fc5f02ffd0
sam201994 <smritigoel93@gmail.com>
2017-03-24 17:29:37 -0700

    stack classs using linked list

---
+++ stacks_queues/stackClass.js
@@ -0,0 +1,50 @@
+class StackNode {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	} 
+}
+
+class Stack {
+	constructor(){
+		this._top = -1;
+		this._head = null;
+	}
+	isEmpty(){
+		return this.size() === 0;
+	}
+	size(){
+		return this._top + 1;
+	}
+	push(value) {
+		let newNode = new StackNode(value);
+		if(this._head === null) {
+			this._head = newNode;
+		}else{
+			newNode.next = this._head;
+			this._head = newNode;
+		}
+		this._top++;
+	}
+	pop(){
+		if(this.isEmpty()) {
+			return "STACK EMPTY - pop"
+		}
+		else {
+			let value = this._head.value;
+			this._head = this._head.next;
+			this._top--;
+			return value;
+		}
+
+	}
+	top(){
+		if(this.isEmpty())
+			return "STACK EMPTY - top";
+		else 
+			return this._head.value;
+	}
+}
+
+
+
